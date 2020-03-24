a25267030b3714d0a7cb2572e399bc705c37f3e9
Brian Ly <thangln1003@gmail.com>
2020-03-24 16:26:37 +0800

    Fixed navbar CSS

--- amazing/src/components/Header/Header.js
+++ amazing/src/components/Header/Header.js
@@ -5,7 +5,7 @@ import NavbarLinks from './NavbarLinks';
 
 const Header = () => {
 	return (
-		<Navbar bg="light" expand="lg">
+		<Navbar className='navbar-default' expand="lg">
 			<Navbar.Brand href="#home">Amazing System</Navbar.Brand>
 			<Navbar.Toggle />
 			<Navbar.Collapse id="basic-navbar-nav">
