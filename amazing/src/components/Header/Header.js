d1c6d5ff4b939d770894dad6a02ac7f731b629aa
Brian Ly <thangln1003@gmail.com>
2020-03-25 02:51:56 +0800

    Update CSS styles

--- amazing/src/components/Header/Header.js
+++ amazing/src/components/Header/Header.js
@@ -1,18 +1,20 @@
 import React from 'react';
-import { Navbar } from 'react-bootstrap';
+import { Container, Navbar } from 'react-bootstrap';
 
 import NavbarLinks from './NavbarLinks';
 
 const Header = () => {
-	return (
-		<Navbar className='navbar-default' expand="lg">
-			<Navbar.Brand href="#home">Amazing</Navbar.Brand>
-			<Navbar.Toggle />
-			<Navbar.Collapse className="justify-content-between" id="basic-navbar-nav">
-				<NavbarLinks />
-			</Navbar.Collapse>
-		</Navbar>
-	);
+  return (
+    <Navbar className="navbar-default" expand="lg">
+      <Container fluid>
+        <Navbar.Brand href="#home">Amazing</Navbar.Brand>
+        <Navbar.Toggle />
+        <Navbar.Collapse className="justify-content-between" id="basic-navbar-nav">
+          <NavbarLinks />
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
 };
 
 export default Header;
--- amazing/src/components/Header/NavbarLinks.js
+++ amazing/src/components/Header/NavbarLinks.js
@@ -2,70 +2,100 @@ import React from 'react';
 import { Nav, NavDropdown } from 'react-bootstrap';
 
 const NavbarLinks = () => {
-	const notification = (
-		<div>
-			<i className="fa fa-globe" />
-			<b className="caret" />
-			<span className="notification">5</span>
-			<p className="hidden-lg hidden-md">Notification</p>
-		</div>
-	);
+  const notification = (
+    <div>
+      <i className="fa fa-globe" />
+      <b className="caret" />
+      <span className="notification">5</span>
+      <p className="hidden-lg hidden-md">Notification</p>
+    </div>
+  );
 
-	return (
-		<>
-			<Nav className="justify-content-start">
-				<Nav.Link href="dashboard">Dashboard</Nav.Link>
-				<Nav.Link href="typography">Typography</Nav.Link>
-				<NavDropdown title="Dropdown" id="basic-nav-dropdown">
-					<NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-					<NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-					<NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-					<NavDropdown.Divider />
-					<NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-				</NavDropdown>
-			</Nav>
-			<Nav className="justify-content-end">
-				<Nav.Link href="#account">Account</Nav.Link>
-				<NavDropdown
-					alignRight
-					title={
-						<div>
-							<i className="fa fa-list" />
-							<p className="d-md-none d-lg-none">
-								More
-								<b className="caret" />
-							</p>
-						</div>
-					}
-					id="basic-nav-dropdown"
-					bsPrefix="dropdown-with-icons dropdown"
-				>
-					<NavDropdown.Item disabled href="#action/3.1">
-						<i className="pe-7s-mail" />
-						Message
-					</NavDropdown.Item>
-					<NavDropdown.Item href="#action/3.2">
-						<i className="pe-7s-help1" />
-						Help Center
-					</NavDropdown.Item>
-					<NavDropdown.Item href="#action/3.3">
-						<i className="pe-7s-tools" />
-						Settings
-					</NavDropdown.Item>
-					<NavDropdown.Divider />
-					<NavDropdown.Item href="#action/3.4">
-						<i className="pe-7s-lock" />
-						Lock Screen
-					</NavDropdown.Item>
-					<NavDropdown.Item href="#action/3.5">
-						<div className="text-danger">
-							<i className="pe-7s-close-circle" /> Log out
-						</div>
-					</NavDropdown.Item>
-				</NavDropdown>
-			</Nav>
-		</>
-	);
+  return (
+    <>
+      <Nav className="justify-content-start">
+        <Nav.Link href="dashboard">Dashboard</Nav.Link>
+        <Nav.Link href="typography">Typography</Nav.Link>
+        <NavDropdown alignRight title="Dropdown" id="basic-nav-dropdown1">
+          <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+          <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+          <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+          <NavDropdown.Divider />
+          <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+        </NavDropdown>
+      </Nav>
+      <Nav className="justify-content-end">
+        <NavDropdown
+          alignRight
+          title={
+            <div>
+              <i className="fa fa-bell-o" />
+              <span className="notification">5</span>
+              <p className="d-md-none d-lg-none">
+                Notifications
+                <b className="caret" />
+              </p>
+            </div>
+          }
+          id="basic-nav-dropdown2"
+          bsPrefix="dropdown-with-icons dropdown"
+        >
+          <NavDropdown.Item href="#action/2.1">
+            Notification 1
+          </NavDropdown.Item>
+          <NavDropdown.Item href="#action/2.2">
+            Notification 2
+          </NavDropdown.Item>
+          <NavDropdown.Item href="#action/2.3">
+            Notification 3
+          </NavDropdown.Item>
+          <NavDropdown.Item href="#action/2.4">
+            Notification 4
+          </NavDropdown.Item>
+          <NavDropdown.Item href="#action/2.5">
+            Another notifications
+          </NavDropdown.Item>
+        </NavDropdown>
+        <NavDropdown
+          alignRight
+          title={
+            <div>
+              <i className="fa fa-list" />
+              <p className="d-md-none d-lg-none">
+                More
+                <b className="caret" />
+              </p>
+            </div>
+          }
+          id="basic-nav-dropdown3"
+          bsPrefix="dropdown-with-icons dropdown"
+        >
+          <NavDropdown.Item disabled href="#action/3.1">
+            <i className="pe-7s-mail" />
+            Message
+          </NavDropdown.Item>
+          <NavDropdown.Item href="#action/3.2">
+            <i className="pe-7s-help1" />
+            Help Center
+          </NavDropdown.Item>
+          <NavDropdown.Item href="#action/3.3">
+            <i className="pe-7s-tools" />
+            Settings
+          </NavDropdown.Item>
+          <NavDropdown.Divider />
+          <NavDropdown.Item href="#action/3.4">
+            <i className="pe-7s-lock" />
+            Lock Screen
+          </NavDropdown.Item>
+          <NavDropdown.Item href="#action/3.5">
+            <div className="text-danger">
+              <i className="pe-7s-close-circle" /> Log out
+            </div>
+          </NavDropdown.Item>
+        </NavDropdown>
+      </Nav>
+    </>
+  );
 };
 
 export default NavbarLinks;
