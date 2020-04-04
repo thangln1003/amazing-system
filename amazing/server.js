f3bc202f3d49801af71715a2841226b9f8aade0e
Brian Ly <thangln1003@gmail.com>
2020-04-04 22:27:31 +0800

    Upload defined routes

--- amazing/server.js
+++ amazing/server.js
@@ -4,6 +4,11 @@ const app = express();
 
 app.get('/', (req, res) => res.send('API Running'));
 
+// Define Routes
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/roles', require('./routes/api/roles'));
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
