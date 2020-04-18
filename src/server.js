cb2bf88341711ff554522ebae301a8409705211d
Brian Ly <thangln1003@gmail.com>
2020-04-19 03:44:24 +0800

    Moved client folder back into src folder

--- src/server.js
+++ src/server.js
@@ -30,7 +30,7 @@ app.use('/api/v1/roles', require('./routes/api/v1/roles'));
 if (process.env.NODE_ENV === 'production') {
 	// Express will serve up production assets
 	// like our main.js file, or main.css file!
-	app.use(expres.static('../client/build'));
+	app.use(expres.static('client/build'));
 
 	// Express will serve up the index.html file
 	// if it doesn't recognize the route
