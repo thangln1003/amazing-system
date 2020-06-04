160d6e372a3922d465b9b4eec79038d3a66fa81d
Brian Ly <thangln1003@gmail.com>
2020-06-04 23:14:32 +0800

    Updated code to async syntax

--- src/client/src/store/actions/roles/roles.actions.js
+++ src/client/src/store/actions/roles/roles.actions.js
@@ -8,92 +8,74 @@ export const UPDATE_ROLE = '[GUS] UPDATE ROLE';
 export const DELETE_ROLE = '[GUS] DELETE ROLE';
 export const LOADING_ROLE = '[GUS] LOADING ROLE';
 
-export const fetchRoles = (data) => {
-  const request = axios.get(`/api/roles`);
+export const fetchRoles = (data) => async (dispatch) => {
+  const response = await axios.get(`/api/roles`);
 
-  return (dispatch) =>
-    request.then((response) => {
-      dispatch({
-        type: FETCH_ROLES,
-        payload: response.data,
-      });
-    });
+  dispatch({
+    type: FETCH_ROLES,
+    payload: response.data,
+  });
 };
 
-export const getRole = (name) => {
-  const request = axios.get(`/api/roles/${name}`);
+export const getRole = (name) => async (dispatch) => {
+  const response = await axios.get(`/api/roles/${name}`);
 
-  return (dispatch) =>
-    request.then((response) =>
-      dispatch({
-        type: GET_ROLE,
-        payload: response.data.result,
-      })
-    );
+  dispatch({
+    type: GET_ROLE,
+    payload: response.data.result,
+  });
 };
 
-export const createRole = (data) => {
-  const request = axios.post(`/api/roles`, data);
+export const createRole = (data) => async (dispatch) => {
+  const response = await axios.post(`/api/roles`, data);
 
-  return (dispatch) =>
-    request.then((response) => {
-      dispatch(
-        Actions.showNotification({
-          message: 'Role is created successfully!',
-          variant: 'success',
-        })
-      );
+  dispatch(
+    Actions.showNotification({
+      message: 'Role is created successfully!',
+      variant: 'success',
+    })
+  );
 
-      return dispatch({
-        type: CREATE_ROLE,
-        payload: response.data.role,
-      });
-    });
+  dispatch({
+    type: CREATE_ROLE,
+    payload: response.data.role,
+  });
 };
 
-export const updateRole = (data) => {
+export const updateRole = (data) => async (dispatch) => {
   const { id } = data;
-  const request = axios.put(`/api/roles/${id}`, data);
+  const response = await axios.put(`/api/roles/${id}`, data);
 
-  return (dispatch) =>
-    request.then((response) => {
-      dispatch(
-        Actions.showNotification({
-          message: 'Role is updated successfully!',
-          variant: 'success',
-        })
-      );
+  dispatch(
+    Actions.showNotification({
+      message: 'Role is updated successfully!',
+      variant: 'success',
+    })
+  );
 
-      return dispatch({
-        type: UPDATE_ROLE,
-        payload: response.data.role,
-      });
-    });
+  dispatch({
+    type: UPDATE_ROLE,
+    payload: response.data.role,
+  });
 };
 
-export const removeRole = (roleId) => {
-  return (dispatch, getState) => {
-    const request = axios.delete(`/api/roles/${roleId}`);
+export const removeRole = (roleId) => async (dispatch, getState) => {
+  await axios.delete(`/api/roles/${roleId}`);
 
-    return request.then((response) =>
-      Promise.all([
-        dispatch(
-          Actions.showNotification({
-            message: 'Role is deleted successfully!',
-            variant: 'success',
-          })
-        ),
-        dispatch({ type: DELETE_ROLE }),
-      ]).then(() => dispatch(fetchRoles()))
-    );
-  };
+  Promise.all([
+    dispatch(
+      Actions.showNotification({
+        message: 'Role is deleted successfully!',
+        variant: 'success',
+      })
+    ),
+    dispatch({ type: DELETE_ROLE }),
+  ]).then(() => dispatch(fetchRoles()));
 };
 
-export const isLoading = (loading) => {
-  return (dispatch) => {
-    return dispatch({
-      type: LOADING_ROLE,
-      payload: loading,
-    });
-  };
+export const isLoading = (loading) => (dispatch) => {
+  return dispatch({
+    type: LOADING_ROLE,
+    payload: loading,
+  });
 };
